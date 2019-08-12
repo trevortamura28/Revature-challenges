f99eac96370639374e6a04509c3224e9e8d6acba
trevortamura28 <53870350+trevortamura28@users.noreply.github.com>
2019-08-12 11:14:13 -0500

    Add files via upload

---
+++ Week1/Week1.Client/Main.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Week1.Domain;
+
+namespace Week1.Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        //White spaces are counted as letters in the Palindrome and must be equal
+        {
+          PalindromeClass palindromeObject = new PalindromeClass();
+          palindromeObject.GetPalindrome();
+          palindromeObject.CheckPalindrome();
+         
+
+        }
+    }
+}
---
+++ Week1/Week1.Domain/Palindrome.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Week1.Domain
+{
+    public class PalindromeClass
+    {
+       string Palindrome;
+       bool StillPalindrome; 
+       public void CheckPalindrome()
+       {
+         int length = Palindrome.Length;
+         int count =0;
+         for (int i = 0; i < length; i++)
+         {
+             StillPalindrome = CheckLetter(Palindrome[i], Palindrome[(length-1)-i]);
+             count++;
+             if (StillPalindrome == false)
+              {
+                break;
+              }
+         }
+         if (StillPalindrome == true)
+         {
+           System.Console.WriteLine("Your word is a palindrome");
+         }
+         else
+          {
+            System.Console.WriteLine("Your word is NOT a palindrome");
+          }
+       }
+
+
+       public bool CheckLetter(char a, char b)
+       {
+         bool result;
+         result = a.Equals(b);
+         return result;
+       }
+
+
+      public void GetPalindrome()
+      {
+        System.Console.WriteLine("Enter a word or phrase: ");
+        Palindrome = Console.ReadLine();
+      }
+    }
+}
